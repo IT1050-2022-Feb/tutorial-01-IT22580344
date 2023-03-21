80d5dd3e58db553613c3e7a1dab15f574357db56
sudila jayasekara <mpsjayasekara@gmail.com  git config --global user.name IT22580344>
2023-03-21 06:19:03 +0530

    Tutorial 01 done

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 
 int main() {
-  
+  float s1,s2,avg;
+  printf("Enter Subject 01 Marks: ");
+  scanf("%f",&s1);
+  printf("Enter Subject 02 Marks: ");
+  scanf("%f",&s2);
+
+  avg = (s1+s2)/2.0;
+  printf("Average is : %f",avg);
   return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+    float distance,amount=0;
+    printf("Input Distance: ");
+    scanf("%f",&distance);
+
+    if(distance<=30){
+        amount = distance*50;
+    }
+    else if(distance>30)
+    {
+        amount = (30*50) + (distance-30)*40;
+    }
+
+    printf("Amount is : %f",amount);
   return 0;
 }
